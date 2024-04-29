5e533a9a1373bb2a58d200930e2698c298b4b24c
Hafiz Shayan <shayanqamar421@gmail.com>
2024-04-30 00:31:16 +0200

    final product

---
+++ README.md
@@ -0,0 +1,2 @@
+# Secret-Coding-Language
+A project has been created in a unique coding language where the user enters three words. If the length of the combined words is greater than three, the language will append the first 3 characters at the beginning and end, and attach the first character to the end of the word. If the length of the combined words is less than or equal to three, the words will be reversed.
---
+++ secret coding language.py
@@ -0,0 +1,31 @@
+import random
+import string
+
+st = input("Please Enter Number : ")
+words= st.split(" ")
+coding = input ("Coding for 1 and Decording for 0 : ")
+coding = True if (coding=="1") else False
+if (coding):
+    nwords=[]
+    for word in words:
+        if(len(word)>=3):
+            r1 = ''.join(random.choice(string.ascii_letters)for i in range(3))
+            r2 = ''.join(random.choice(string.ascii_letters)for i in range(3))
+            stnew = r1 + word[1:] + word[0] + r2
+            nwords.append(stnew)
+        else :
+            nwords.append(word[::-1])
+        print(" ".join(nwords))
+         
+else :
+    nwords=[]
+    for word in words:
+        if(len(word)>=3): 
+            stnew = word[3:-3]
+            stnew = stnew[-1] + stnew[:-1]
+            nwords.append(stnew)
+        else :
+            nwords.append(word[::-1])
+        print(" ".join(nwords))
+
+        
